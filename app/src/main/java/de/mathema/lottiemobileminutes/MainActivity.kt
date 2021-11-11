06254c5f30ca18a232418edc6517112dd91a7a53
NilsRamon <nils.vinmann@mathema.de>
2021-11-11 16:21:02 +0100

    added switch button

--- app/src/main/java/de/mathema/lottiemobileminutes/MainActivity.kt
+++ app/src/main/java/de/mathema/lottiemobileminutes/MainActivity.kt
@@ -1,11 +1,37 @@
 package de.mathema.lottiemobileminutes
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import com.airbnb.lottie.LottieAnimationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var fab: FloatingActionButton
+    lateinit var animationView: LottieAnimationView
+    var wasClicked = false
+
+
+    @RequiresApi(Build.VERSION_CODES.Q)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        fab = findViewById(R.id.fab)
+        animationView = findViewById(R.id.animationView)
+        fab.setOnClickListener {
+            wasClicked = if (wasClicked) {
+                fab.setImageResource(R.drawable.ic_baseline_switch_left_24)
+                animationView.setAnimation(R.raw.animationbike)
+                animationView.playAnimation()
+                false
+            } else {
+                fab.setImageResource(R.drawable.ic_baseline_switch_right_24)
+                animationView.setAnimation(R.raw.animation)
+                animationView.playAnimation()
+                true
+            }
+        }
     }
 }
